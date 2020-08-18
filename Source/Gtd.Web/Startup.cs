77f5b028fce0d661f905f295c641310fd14cbb57
John Moore <john@johnclaytonmoore.com>
2020-08-18 16:49:34 -0500

    perform db migration on startup

--- Source/Gtd.Web/Startup.cs
+++ Source/Gtd.Web/Startup.cs
@@ -54,8 +54,10 @@ namespace Gtd.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext ctx)
         {
+            ctx.Database.Migrate();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
